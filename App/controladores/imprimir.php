b971f1503ba254fbfdf6b6e5ed2de5386fc4e859
CarlosSamuel <csamuelrox@gmail.com>
2023-09-26 16:14:17 -0500

    Try catchs

--- App/controladores/imprimir.php
+++ App/controladores/imprimir.php
@@ -9,15 +9,30 @@
 
     function imprimir($id){
 
-        $con = Connection::getInstance()->getConnection();
-
-        $quer = $con->query("SELECT * FROM Facturas WHERE vtaid = " . $id . ";");
-    
-        $row = $quer->fetch_assoc();
-
-        utf8_encode_array($row);
-
-        $elementos = explode("#", $row['Embalaje']);    
+        try {
+            $con = Connection::getInstance()->getConnection();
+        } catch (Exception $e) {
+            echo 'Error en la conexión: ' . $e->getMessage();
+        }
+        
+        try {
+            $quer = $con->query("SELECT * FROM Facturas WHERE vtaid = " . $id . ";");
+        } catch (Exception $e) {
+            echo 'Error en la consulta SQL: ' . $e->getMessage();
+        }
+        
+        try {
+            $row = $quer->fetch_assoc();
+            utf8_encode_array($row);
+        } catch (Exception $e) {
+            echo 'Error al recuperar datos o al ejecutar utf8_encode_array: ' . $e->getMessage();
+        }
+        
+        try {
+            $elementos = explode("#", $row['Embalaje']);
+        } catch (Exception $e) {
+            echo 'Error al dividir la cadena: ' . $e->getMessage();
+        }
 
         $i = 1;
     
@@ -34,43 +49,44 @@
                     
                     $j = 1;
 
-                    while ($j <= $valor){
-                        $template = new TemplateProcessor('../plantillas/etiqueta.docx');
-
-                        $template->setValue('cliente_nombre', $row['TerNom']);
-                        $template->setValue('cliente_telefono', $row['TerTel']);
-                        $template->setValue('cliente_direccion', $row['TerDir']);
-                        $template->setValue('observaciones', $row['ObservacionesVer']);
-                        $template->setValue('e', $clave);
-                        $template->setValue('n', $j);
-                        $template->setValue('t', $valor);
-    
-                        $inputFile = '../documentos/documento_generado'.$i.'.docx';
-                        $outputFile = '../documentos/documento_generado'.$i.'.pdf';
-                
-                        $template->saveAs($inputFile);
-    
-                        $command = "libreoffice --convert-to pdf $inputFile  --outdir " . dirname($outputFile);
-
-                        $output = [];
-                        $returnCode = 0;
-                        
-                        exec($command, $output, $returnCode);
-
-                        if (!(unlink($inputFile))) {
-                            echo "No se puedo borrar: " . $inputFile;
+                    try {
+                        while ($j <= $valor) {
+                            $template = new TemplateProcessor('../plantillas/etiqueta.docx');
+                    
+                            $template->setValue('cliente_nombre', $row['TerNom']);
+                            $template->setValue('cliente_telefono', $row['TerTel']);
+                            $template->setValue('cliente_direccion', $row['TerDir']);
+                            $template->setValue('observaciones', $row['ObservacionesVer']);
+                            $template->setValue('e', $clave);
+                            $template->setValue('n', $j);
+                            $template->setValue('t', $valor);
+                    
+                            $inputFile = '../documentos/documento_generado'.$i.'.docx';
+                            $outputFile = '../documentos/documento_generado'.$i.'.pdf';
+                    
+                            $template->saveAs($inputFile);
+                    
+                            $command = "libreoffice --convert-to pdf $inputFile  --outdir " . dirname($outputFile);
+                    
+                            $output = [];
+                            $returnCode = 0;
+                    
+                            exec($command, $output, $returnCode);
+                    
+                            if (!(unlink($inputFile))) {
+                                echo "No se pudo borrar: " . $inputFile;
+                            }
+                    
+                            $j++;
+                            $i++;
                         }
-                        $j++;
-                        $i++;
+                    } catch (Exception $e) {
+                        echo 'Error en el bucle while: ' . $e->getMessage();
                     }
                 }
             }
         }
 
     }
-
-    // imprimir(230705);
-
-
 
 ?>
